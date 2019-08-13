10e4b0ed43c7a5e1c8a29715aef175899d8907fb
System Administrator <sysadm@sashi06>
2019-08-13 14:22:13 +0530

    commit

--- search/search.py
+++ search/search.py
@@ -88,28 +88,28 @@ def depthFirstSearch(problem):
     print("Start's successors:", problem.getSuccessors(problem.getStartState()))
     """
     "*** YOUR CODE HERE ***"
-
+    """
     #print("Start:", problem.getStartState())
     #print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
     #print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-
+    
     distance_list = []
 
     my_stack = util.Stack()
     current_state = problem.getStartState()
     #print("Started at => ",current_state)
     visited = []
-    visited.append(current_state)
-    while problem.isGoalState(current_state) == False:
+    #visited.append(current_state)
+    while problem.isGoalState(current_state) == False: 
       
       #print("State obtained => ",distance_list)
       get_sucessors = problem.getSuccessors(current_state)
-      get_sucessors.reverse()
+      #get_sucessors.reverse()
       #print("Successors => ",get_sucessors)
       #my_stack.showStack()
       for items_coord in get_sucessors:
         if items_coord[0] not in visited:
-          visited.append(items_coord[0])
+          
           my_stack.push(items_coord)
 
       
@@ -118,6 +118,7 @@ def depthFirstSearch(problem):
       else:
         # my_stack.showStack()
         current_state_tuple = my_stack.pop()
+        visited.append(current_state_tuple[0])
         # if my_stack.inMyStack(current_state_tuple) == True:
         #   print("********* it's present")
         # my_stack.showStack()
@@ -129,6 +130,7 @@ def depthFirstSearch(problem):
     #util.raiseNotDefined()
     #print("State obtained => ",distance_list)
     #print("Distance list => ",distance_list)
+    print(distance_list)
     d_list = []
     from game import Directions
     s = Directions.SOUTH
@@ -144,7 +146,67 @@ def depthFirstSearch(problem):
           d_list.append(n)
         if item == 'w':
           d_list.append(w)
+    util.pause()
     return d_list
+
+    """
+    path = []
+    visited = []
+    parent = {}
+    current_state = problem.getStartState()
+    x = current_state
+    y = (1,1)
+    def dfs(current_state):
+      visited.append(current_state)
+      get_sucessors = problem.getSuccessors(current_state)
+
+      for items_coord in get_sucessors:
+        if items_coord[0] not in visited:
+          if problem.isGoalState(items_coord[0]):
+            y = items_coord[0]
+            parent[items_coord[0]] = current_state
+            return
+            
+          parent[items_coord[0]] = current_state
+          dfs(items_coord[0])
+    dfs(current_state)
+    #print(parent)
+    print(y)
+    print(x)
+    while parent[y] != x:
+      z = parent[y]
+      if y[0] == z[0]:
+        if z[1] > y[1]:
+          path.append('s')
+        else:
+          path.append('n')
+      else:
+        if y[1] > z[1]:
+          path.append('e')
+        else:
+          path.append('w')
+      y = z
+    path.append('s')
+    path.reverse()
+    d_list = []
+    from game import Directions
+    s = Directions.SOUTH
+    w = Directions.WEST
+    n = Directions.NORTH
+    e = Directions.EAST
+    for item in path:
+        if item == 's':
+          d_list.append(s)
+        if item == 'e':
+          d_list.append(e)
+        if item == 'n':
+          d_list.append(n)
+        if item == 'w':
+          d_list.append(w)
+    print(path)
+    return d_list
+
+    
 
 def breadthFirstSearch(problem):
     """Search the shallowest nodes in the search tree first."""
